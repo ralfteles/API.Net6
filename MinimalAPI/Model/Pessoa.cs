dfc5ac4d64cd556e2df6806dd807a1a8a8901b7b
Rafael <ralf_teles@hotmail.com>
2021-11-06 00:05:50 -0300

    Adicionando contrutor da classe pessoa

--- MinimalAPI/Model/Pessoa.cs
+++ MinimalAPI/Model/Pessoa.cs
@@ -2,8 +2,15 @@
 {
     public class Pessoa
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Sobrenome { get; set; }
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+
+        public Pessoa(int id, string nome, string sobrenome)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
     }
 }
