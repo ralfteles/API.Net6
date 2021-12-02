aff7b2889cb8c5d7e81e35a1d2eb3dfef59fea78
Rafael <ralf_teles@hotmail.com>
2021-12-02 11:18:22 -0300

    Isolando endpoint e swagger

---
+++ MinimalAPI/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace MinimalAPI.Configuration
+{
+    public class SwaggerConfiguration
+    {
+        public static void AddSwagger(WebApplication app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API v1");
+            });
+
+        }
+    }
+}
---
+++ MinimalAPI/EndPoints/PessoaEndPointConfig.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Model;
+
+namespace MinimalAPI.EndPoints
+{
+    public class PessoaEndPointConfig
+    {
+        public static void AddEndPoints(WebApplication app)
+        {
+            app.MapGet("/pessoas", async (PessoaDbContext dbContext) => await dbContext.Pessoas.ToListAsync());
+
+            app.MapGet("/pessoas/{id}", async (int id, PessoaDbContext dbContext) => await dbContext.Pessoas.FirstOrDefaultAsync(a => a.Id == id));
+
+            app.MapPost("/pessoas", async (Pessoa pessoa, PessoaDbContext dbContext) =>
+            {
+                dbContext.Pessoas.Add(pessoa);
+                await dbContext.SaveChangesAsync();
+
+                return pessoa;
+            });
+
+            app.MapPut("/pessoas/{id}", async (int id, Pessoa pessoa, PessoaDbContext dbContext) =>
+            {
+                dbContext.Entry(pessoa).State = EntityState.Modified;
+                await dbContext.SaveChangesAsync();
+                return pessoa;
+            });
+
+            app.MapDelete("/pessoas/{id}", async (int id, PessoaDbContext dbContext) =>
+            {
+                var pessoa = await dbContext.Pessoas.FirstOrDefaultAsync(a => a.Id == id);
+
+                dbContext.Pessoas.Remove(pessoa);
+                await dbContext.SaveChangesAsync();
+                return;
+            });
+        }
+    }
+}
